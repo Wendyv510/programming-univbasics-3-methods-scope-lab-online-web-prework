51d962cbf897bc2037262defdf6d669c57cb40be
Wendy Harris <wendyv510@hotmail.com>
2019-09-20 22:53:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,3 +1,5 @@
+phrase = "It's-a me, Mario!" 
+
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
@@ -6,3 +8,21 @@ end
 def toadstool
   puts status
 end
+
+describe "mario"do
+  it "puts out 'It's a me, Mario!'"do
+    expect {mario}.to output ("It's-a me, Mario!\n").to_stdout
+  end
+end
+
+describe "toadstool"do
+  it "puts out 'Thank you Mario! But our Princess Is In Another Castle!'"do
+    expect {toadstool}.to output ("Than you Mario! But our Princess is in another castle!\n").to_stdout
+  end
+end
+
+describe "link"do
+  it "puts out 'It's Dangerous To Go Alone! Take this.'"do
+    expect {link}.to output ("It's Dangerous To Go Alone! Take This.\n").to_stdout
+  end
+  
